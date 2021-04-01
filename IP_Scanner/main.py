02c1127fda501d7fe9d9c8afc790b7e58b82bf9f
Kenny Ta <55076129+takenny@users.noreply.github.com>
2021-04-01 15:00:20 -0700

    lmfao
    lmfao

--- IP_Scanner/main.py
+++ IP_Scanner/main.py
@@ -1,3 +1,31 @@
+import networkscan
 import socket
 
-print([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")])
+
+def isOpen(ip, port):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        s.connect((ip, int(port)))
+        s.shutdown(2)
+        return True
+    except:
+        return False
+
+
+if __name__ == '__main__':
+
+    # Define the network to scan
+    my_network = "10.0.0.0/24"
+
+    # Create the object
+    my_scan = networkscan.Networkscan(my_network)
+    # Run the scan of hosts using pings
+    my_scan.run()
+    counter = 0
+    # Display the IP address of all the hosts found
+    for i in my_scan.list_of_hosts_found:
+        counter = counter + 1
+        if (isOpen(i, 80)):
+            print("port %d is open", i)
+
+    print(counter)
